4dd40a7cfb5315eef373c7886a4faa00ff691dc7
geteatpos <geteatpos@gmail.com>
2025-02-13 13:52:52 -0500

    server initial

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const twilio = require('twilio');
+
+const app = express();
+app.use(express.json());
+
+const TWILIO_ACCOUNT_SID = process.env.TWILIO_ACCOUNT_SID;
+const TWILIO_AUTH_TOKEN = process.env.TWILIO_AUTH_TOKEN;
+const client = twilio(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
+
+app.post('/incoming-call', async (req, res) => {
+    const twiml = new twilio.twiml.VoiceResponse();
+    twiml.say("Hola, bienvenido al restaurante. ¿Cómo puedo ayudarle?");
+    res.type('text/xml');
+    res.send(twiml.toString());
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
